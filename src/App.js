019264e0a539bf3906cd09cdd953a6131736a4c1
DBusie <132249012+DBusie@users.noreply.github.com>
2023-10-06 18:28:46 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
 
       <footer>
         This project was coded by Busi Shumba and is {""}
-        <a href="https://github.com/DBusie/weather-app-react" target="_blank">
+        <a
+          href="https://github.com/DBusie/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on Github
         </a>
       </footer>
