ce51960bc77ea84aed62191e175f16008194382c
DBusie <132249012+DBusie@users.noreply.github.com>
2023-10-06 16:58:35 +0200

    Added Git hub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by Busi Shumba and is {""}
+        <a href="https://github.com/DBusie/weather-app-react" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
