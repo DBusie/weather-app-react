d052273fcc18674eb634b6ac2d8008d597dfb5a4
DBusie <132249012+DBusie@users.noreply.github.com>
2023-10-25 17:14:19 +0200

    updated App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,22 @@ function App() {
       <div className="container">
         <Weather defaultCity="lisbon" />
         <footer>
-          This website was coded by Busi Shumba and is {""}
+          This project was coded by Busi Shumba and is {""}
           <a
             href="https://github.com/DBusie/weather-app-react"
             target="_blank"
             rel="noreferrer"
           >
             open-sourced on Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://voluble-bubblegum-59654e.netlify.app/"
+            alt="netlify-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            netlify
           </a>
         </footer>
       </div>
